885c4ff0c2796ba34c6008f59ea0122aeb386ce5
TheRealSal <75696327+TheRealSal@users.noreply.github.com>
2023-08-11 11:32:55 -0400

    Fixed edit pages for student and employee

--- employee/edit.php
+++ employee/edit.php
@@ -8,24 +8,71 @@ $statement->bindParam(":employeeID", $employeeID);
 $statement->execute();
 $employee = $statement->fetch(PDO::FETCH_ASSOC);
 
-if (isset($_POST["employeeID"]) && isset($_POST["personID"]) && isset($_POST["employeeType"])) {
-    $updateStatement = $conn->prepare("UPDATE employee 
-                                       SET personID = :personID, 
-                                           employeeType = :employeeType 
-                                       WHERE employeeID = :employeeID");
 
-    $updateStatement->bindParam(':employeeID', $employeeID);
-    $updateStatement->bindParam(':personID', $_POST["personID"]);
-    $updateStatement->bindParam(':employeeType', $_POST["employeeType"]);
+if (isset($_POST["personID"])) {
+    $personID = $_POST["personID"];
+    $employeeID = $_POST["employeeID"];
+    $employeeType = $_POST["employeeType"];
 
-    if ($updateStatement->execute()) {
-        header("Location: .");
-        exit();
+    // Update data in the personalInformation table
+    $personalInfoStatement = $conn->prepare("UPDATE personalInformation 
+                                             SET firstName = :firstName, 
+                                                 lastName = :lastName, 
+                                                 dateOfBirth = :dateOfBirth, 
+                                                 postalCode = :postalCode, 
+                                                 address = :address, 
+                                                 phoneNumber = :phoneNumber, 
+                                                 email = :email, 
+                                                 citizenship = :citizenship, 
+                                                 medicareNumber = :medicareNumber, 
+                                                 medicareExpirationDate = :medicareExpirationDate 
+                                             WHERE personID = :personID");
+
+    $personalInfoStatement->bindParam(':personID', $personID);
+    $personalInfoStatement->bindParam(':firstName', $_POST["firstName"]);
+    $personalInfoStatement->bindParam(':lastName', $_POST["lastName"]);
+    $personalInfoStatement->bindParam(':dateOfBirth', $_POST["dateOfBirth"]);
+    $personalInfoStatement->bindParam(':postalCode', $_POST["postalCode"]);
+    $personalInfoStatement->bindParam(':address', $_POST["address"]);
+    $personalInfoStatement->bindParam(':phoneNumber', $_POST["phoneNumber"]);
+    $personalInfoStatement->bindParam(':email', $_POST["email"]);
+    $personalInfoStatement->bindParam(':citizenship', $_POST["citizenship"]);
+    $personalInfoStatement->bindParam(':medicareNumber', $_POST["medicareNumber"]);
+    $personalInfoStatement->bindParam(':medicareExpirationDate', $_POST["medicareExpirationDate"]);
+
+    if ($personalInfoStatement->execute()) {
+        // Update data in the employee table
+        $employeeStatement = $conn->prepare("UPDATE employee 
+                                             SET employeeType = :employeeType 
+                                             WHERE employeeID = :employeeID");
+
+        $employeeStatement->bindParam(':employeeType', $employeeType);
+        $employeeStatement->bindParam(':employeeID', $employeeID);
+
+        if ($employeeStatement->execute()) {
+            header("Location: .");
+        } else {
+            echo "Error updating employee.";
+        }
     } else {
-        echo "Update failed.";
+        echo "Error updating personal information.";
+    }
+} else {
+    $employeeID = $_GET["employeeID"];
+
+    // Retrieve employee and personal information data for pre-filling the form
+    $statement = $conn->prepare("SELECT * FROM employee 
+                                 JOIN personalInformation AS pi ON employee.personID = pi.personID 
+                                 WHERE employee.employeeID = :employeeID");
+    $statement->bindParam(":employeeID", $employeeID);
+    $statement->execute();
+    $employee = $statement->fetch(PDO::FETCH_ASSOC);
+
+    if (!$employee) {
+        echo "Employee not found.";
+        exit();
     }
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -42,16 +89,44 @@ if (isset($_POST["employeeID"]) && isset($_POST["personID"]) && isset($_POST["em
 
     <h1>Edit Employee</h1>
     <form action="./edit.php?employeeID=<?= $employeeID ?>" method="post">
+        <label for="personID">Person ID</label>
+        <input type="number" name="personID" id="personID" value="<?= $employee["personID"] ?>">
 
-        <label for="personID">Person ID</label> <br>
-        <input type="text" name="personID" id="personID" value="<?= $employee["personID"] ?>"> <br>
+        <label for="firstName">First Name</label>
+        <input type="text" name="firstName" id="firstName" value="<?= $employee["firstName"] ?>">
 
-        <label for="employeeType">Employee Type</label> <br>
-        <input type="text" name="employeeType" id="employeeType" value="<?= $employee["employeeType"] ?>"> <br>
+        <label for="lastName">Last Name</label>
+        <input type="text" name="lastName" id="lastName" value="<?= $employee["lastName"] ?>">
+
+        <label for="dateOfBirth">Date of Birth</label>
+        <input type="date" name="dateOfBirth" id="dateOfBirth" value="<?= $employee["dateOfBirth"] ?>">
+
+        <label for="postalCode">Postal Code</label>
+        <input type="text" name="postalCode" id="postalCode" value="<?= $employee["postalCode"] ?>">
+
+        <label for="address">Address</label>
+        <input type="text" name="address" id="address" value="<?= $employee["address"] ?>">
+
+        <label for="phoneNumber">Phone Number</label>
+        <input type="text" name="phoneNumber" id="phoneNumber" value="<?= $employee["phoneNumber"] ?>">
+
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" value="<?= $employee["email"] ?>">
+
+        <label for="citizenship">Citizenship</label>
+        <input type="text" name="citizenship" id="citizenship" value="<?= $employee["citizenship"] ?>">
+
+        <label for="medicareNumber">Medicare Number</label>
+        <input type="text" name="medicareNumber" id="medicareNumber" value="<?= $employee["medicareNumber"] ?>">
+
+        <label for="medicareExpirationDate">Medicare Expiration Date</label>
+        <input type="date" name="medicareExpirationDate" id="medicareExpirationDate" value="<?= $employee["medicareExpirationDate"] ?>">
+
+        <label for="employeeType">Employee Type</label>
+        <input type="text" name="employeeType" id="employeeType" value="<?= $employee["employeeType"] ?>">
 
         <input type="hidden" name="employeeID" value="<?= $employeeID ?>">
         <input type="submit" value="Update">
-
     </form>
 </body>
 
--- student/edit.php
+++ student/edit.php
@@ -3,30 +3,63 @@ require_once '../database.php';
 
 $studentID = $_GET["studentID"];
 
-$statement = $conn->prepare("SELECT * FROM student WHERE student.studentID = :studentID");
+$statement = $conn->prepare("SELECT * FROM student JOIN personalInformation AS pi ON student.personID=pi.personID WHERE student.studentID = :studentID");
 $statement->bindParam(":studentID", $studentID);
 $statement->execute();
 $student = $statement->fetch(PDO::FETCH_ASSOC);
 
-if (isset($_POST["studentID"]) && isset($_POST["personID"]) && isset($_POST["currentLevel"])) {
-    $updateStatement = $conn->prepare("UPDATE student 
-                                       SET personID = :personID, 
-                                           currentLevel = :currentLevel 
-                                       WHERE studentID = :studentID");
+if (isset($_POST["personID"])) {
+    $personID = $_POST["personID"];
+    $studentID = $_POST["studentID"];
 
-    $updateStatement->bindParam(':studentID', $studentID);
-    $updateStatement->bindParam(':personID', $_POST["personID"]);
-    $updateStatement->bindParam(':currentLevel', $_POST["currentLevel"]);
+    // Update data in the personalInformation table
+    $personalInfoStatement = $conn->prepare("UPDATE personalInformation 
+                                             SET firstName = :firstName, 
+                                                 lastName = :lastName, 
+                                                 dateOfBirth = :dateOfBirth, 
+                                                 postalCode = :postalCode, 
+                                                 address = :address, 
+                                                 phoneNumber = :phoneNumber, 
+                                                 email = :email, 
+                                                 citizenship = :citizenship, 
+                                                 medicareNumber = :medicareNumber, 
+                                                 medicareExpirationDate = :medicareExpirationDate 
+                                             WHERE personID = :personID");
 
-    if ($updateStatement->execute()) {
-        header("Location: .");
-        exit();
+    $personalInfoStatement->bindParam(':personID', $personID);
+    $personalInfoStatement->bindParam(':firstName', $_POST["firstName"]);
+    $personalInfoStatement->bindParam(':lastName', $_POST["lastName"]);
+    $personalInfoStatement->bindParam(':dateOfBirth', $_POST["dateOfBirth"]);
+    $personalInfoStatement->bindParam(':postalCode', $_POST["postalCode"]);
+    $personalInfoStatement->bindParam(':address', $_POST["address"]);
+    $personalInfoStatement->bindParam(':phoneNumber', $_POST["phoneNumber"]);
+    $personalInfoStatement->bindParam(':email', $_POST["email"]);
+    $personalInfoStatement->bindParam(':citizenship', $_POST["citizenship"]);
+    $personalInfoStatement->bindParam(':medicareNumber', $_POST["medicareNumber"]);
+    $personalInfoStatement->bindParam(':medicareExpirationDate', $_POST["medicareExpirationDate"]);
+
+    if ($personalInfoStatement->execute()) {
+        // Update data in the student table
+        $currentLevel = $_POST["currentLevel"];
+
+        $studentStatement = $conn->prepare("UPDATE student 
+                                             SET currentLevel = :currentLevel 
+                                             WHERE studentID = :studentID");
+
+        $studentStatement->bindParam(':studentID', $studentID);
+        $studentStatement->bindParam(':currentLevel', $currentLevel);
+
+        if ($studentStatement->execute()) {
+            header("Location: .");
+        } else {
+            echo "Error updating student.";
+        }
     } else {
-        echo "Update failed.";
+        echo "Error updating personal information.";
     }
 }
-
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -42,16 +75,44 @@ if (isset($_POST["studentID"]) && isset($_POST["personID"]) && isset($_POST["cur
 
     <h1>Edit Student</h1>
     <form action="./edit.php?studentID=<?= $studentID ?>" method="post">
+        <label for="personID">Person ID</label>
+        <input type="number" name="personID" id="personID" value="<?= $student["personID"] ?>">
 
-        <label for="personID">Person ID</label> <br>
-        <input type="text" name="personID" id="personID" value="<?= $student["personID"] ?>"> <br>
+        <label for="firstName">First Name</label>
+        <input type="text" name="firstName" id="firstName" value="<?= $student["firstName"] ?>">
 
-        <label for="currentLevel">Current Level</label> <br>
-        <input type="text" name="currentLevel" id="currentLevel" value="<?= $student["currentLevel"] ?>"> <br>
+        <label for="lastName">Last Name</label>
+        <input type="text" name="lastName" id="lastName" value="<?= $student["lastName"] ?>">
+
+        <label for="dateOfBirth">Date of Birth</label>
+        <input type="date" name="dateOfBirth" id="dateOfBirth" value="<?= $studentData["dateOfBirth"] ?>">
+
+        <label for="postalCode">Postal Code</label>
+        <input type="text" name="postalCode" id="postalCode" value="<?= $studentData["postalCode"] ?>">
+
+        <label for="address">Address</label>
+        <input type="text" name="address" id="address" value="<?= $studentData["address"] ?>">
+
+        <label for="phoneNumber">Phone Number</label>
+        <input type="text" name="phoneNumber" id="phoneNumber" value="<?= $student["phoneNumber"] ?>">
+
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" value="<?= $student["email"] ?>">
+
+        <label for="citizenship">Citizenship</label>
+        <input type="text" name="citizenship" id="citizenship" value="<?= $student["citizenship"] ?>">
+
+        <label for="medicareNumber">Medicare Number</label>
+        <input type="text" name="medicareNumber" id="medicareNumber" value="<?= $student["medicareNumber"] ?>">
+
+        <label for="medicareExpirationDate">Medicare Expiration Date</label>
+        <input type="date" name="medicareExpirationDate" id="medicareExpirationDate" value="<?= $student["medicareExpirationDate"] ?>">
+
+        <label for="currentLevel">Current Level</label>
+        <input type="text" name="currentLevel" id="currentLevel" value="<?= $student["currentLevel"] ?>">
 
         <input type="hidden" name="studentID" value="<?= $studentID ?>">
         <input type="submit" value="Update">
-
     </form>
 </body>
 
